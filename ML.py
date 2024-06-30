55abe11aaf861be7ceeffaaacb67081b4a2534c8
IgorLunyov <igorpadgig@gmail.com>
2024-06-30 10:18:59 +0300

    Installing needed packages

--- ML.py
+++ ML.py
@@ -1,1 +1,8 @@
-print('This is the second push test')
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import nltk
+from nltk.corpus import stopwords
